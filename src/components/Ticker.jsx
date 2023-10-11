e0f49099e088a5d9ab79443c4729c8b62802feb4
OlivierRecher <recher.olivier@outlook.fr>
2023-10-11 19:04:18 +0200

    feat(ticker) ticker finish

--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -12,66 +12,35 @@ class Ticker extends React.Component {
                 'Symfony',
                 'Tailwindcss',
                 'Flutter',
-                'Nuxt.js'
+                'Nuxt.js',
             ]
         }
     }
     componentDidMount() {
-      // call api or anything
       const ticker = document.querySelector('.ticker');
       const list = document.querySelector('.ticker__list');
       const clone = list.cloneNode(true);
 
-      ticker.append(clone)
-      console.log('componentDidMount')
+      ticker.append(clone);
     }
     render() {
         return (
-            <div className="ticker">
-                <div className="ticker__list">
-                    {this.state.ticker.map((item, index) => (
-                        <div className="ticker__item" key={index}>{item}</div>
-                    ))}
-                </div>
-            </div>
+            <div class="h-28 flex flex-row">
+                <div class="column-blur relative left-0 w-1/3 bg-gradient-to-r from-white from-90% z-10"></div>
+
+                    {/* Ticker */}
+                    <div className="ticker flex justify-center items-center h-12 bg-white mx-auto w-1/3">
+                        <div className="ticker__list flex relative left-[55%] mt-16">
+                            {this.state.ticker.map((item, index) => (
+                                <div className="ticker__item font-bold whitespace-nowrap mr-12 text-2xl text-[#07042f]" key={index}>{item}</div>
+                            ))}
+                        </div>
+                    </div>
+
+                <div class="column-blur relative right-0 w-1/3 bg-gradient-to-l from-white from-90% z-10"></div>
+	        </div>
         )
     }
 }
-
-// const ticker = () => {
-//     // TODO : variable a null 
-//     // const ticker = document.querySelector('.ticker');
-//     // const list = document.querySelector('.ticker__list');
-//     // const clone = list.cloneNode(true);
-
-// // ticker.append(clone)
-//   return (
-//     // <div class="ticker-wrap">
-//         // <div class="ticker">
-//         //     <div class="ticker__item">JavaScript</div>
-//         //     <div class="ticker__item">PHP</div>
-//         //     <div class="ticker__item">React.js</div>
-//         //     <div class="ticker__item">Vue.js</div>
-//         //     <div class="ticker__item">Symfony</div>
-//         //     <div class="ticker__item">Tailwindcss</div>
-//         //     <div class="ticker__item">Flutter</div>
-//         //     <div class="ticker__item">Nuxt.js</div>
-//         // </div> 
-//     // </div> 
-//     <div class="ticker">
-//         <div class="ticker__list">
-//             <div class="ticker__item">JavaScript</div>
-//             <div class="ticker__item">PHP</div>
-//             <div class="ticker__item">React.js</div>
-//             <div class="ticker__item">Vue.js</div>
-//             <div class="ticker__item">Symfony</div>
-//             <div class="ticker__item">Tailwindcss</div>
-//             <div class="ticker__item">Flutter</div>
-//             <div class="ticker__item">Nuxt.js</div>
-            
-//         </div>
-//     </div>
-//   )
-// }
 
 export default Ticker
