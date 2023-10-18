5b0c084cdb77b08bb9a2264076a3e061e2c1183d
OlivierRecher <recher.olivier@outlook.fr>
2023-10-18 13:45:35 +0200

    feat(darkmode) set darkmode button into component

--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -25,19 +25,19 @@ class Ticker extends React.Component {
     }
     render() {
         return (
-            <div class="h-28 flex flex-row">
-                <div class="column-blur relative left-0 w-1/3 bg-gradient-to-r from-white from-90% z-10"></div>
+            <div className="h-28 flex flex-row">
+                <div className="column-blur relative z-10 left-0 w-1/3 bg-gradient-to-r from-white from-90% dark:from-[#121212]"></div>
 
                     {/* Ticker */}
-                    <div className="ticker flex justify-center items-center h-12 bg-white mx-auto w-1/3">
+                    <div className="ticker flex justify-center items-center h-12 mx-auto w-1/3">
                         <div className="ticker__list flex relative left-[55%] mt-16">
                             {this.state.ticker.map((item, index) => (
-                                <div className="ticker__item font-bold whitespace-nowrap mr-12 text-2xl text-[#07042f]" key={index}>{item}</div>
+                                <div className="ticker__item font-bold whitespace-nowrap mr-12 text-2xl text-[#07042f] dark:text-[#fff]" key={index}>{item}</div>
                             ))}
                         </div>
                     </div>
 
-                <div class="column-blur relative right-0 w-1/3 bg-gradient-to-l from-white from-90% z-10"></div>
+                <div className="column-blur relative z-10 right-0 w-1/3 bg-gradient-to-l from-white from-90% dark:from-[#121212]"></div>
 	        </div>
         )
     }
