05201f6e827bd8c9a638f70a3699807bccf1a906
OlivierRecher <recher.olivier@outlook.fr>
2023-11-10 09:25:24 +0100

    feat(mobilResponsive): text size and ticker ajust

--- src/components/Ticker.jsx
+++ src/components/Ticker.jsx
@@ -26,18 +26,18 @@ class Ticker extends React.Component {
     render() {
         return (
             <div className="h-28 flex flex-row">
-                <div className="column-blur relative z-10 left-0 w-1/3 bg-gradient-to-r from-white from-90% dark:from-[#121212]"></div>
+                <div className="column-blur relative z-10 left-0 sm:w-1/3 w-1/5 bg-gradient-to-r from-white from-90% dark:from-[#121212]"></div>
 
                     {/* Ticker */}
                     <div className="ticker flex justify-center items-center h-12 mx-auto w-1/3">
-                        <div className="ticker__list flex relative left-[55%] mt-16">
+                        <div className="ticker__list flex relative sm:left-[55%] left-[95%] mt-16">
                             {this.state.ticker.map((item, index) => (
-                                <div className="ticker__item font-bold whitespace-nowrap mr-12 text-2xl text-[#07042f] dark:text-[#fff]" key={index}>{item}</div>
+                                <div className="ticker__item font-bold whitespace-nowrap mr-12 sm:text-2xl text-[#07042f] dark:text-[#fff]" key={index}>{item}</div>
                             ))}
                         </div>
                     </div>
 
-                <div className="column-blur relative z-10 right-0 w-1/3 bg-gradient-to-l from-white from-90% dark:from-[#121212]"></div>
+                <div className="column-blur relative z-10 right-0 sm:w-1/3 w-1/5 bg-gradient-to-l from-white from-90% dark:from-[#121212]"></div>
 	        </div>
         )
     }
